20fc0a9966e25ff43f92faa72a3a1b2a768756dd
Snibaw <Snibaw@gmail.com>
2023-11-26 09:34:00 +0100

    Prefab portal

--- Assets/Scripts/LoadSceneOnTrigger.cs
+++ Assets/Scripts/LoadSceneOnTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -5,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class LoadSceneOnTrigger : MonoBehaviour
 {
     public string sceneToLoad; // Drag and drop the scene asset in the Inspector
-    public Transform player;
+    private Transform player;
+    private GameObject particleSystem;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     void Update()
     {
         // Rotate the camera every frame so it keeps looking at the target
         transform.LookAt(player);
+        
     }
 
         private void OnTriggerEnter(Collider other)
