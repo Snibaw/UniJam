72aee52f9b660f42683412e2528bf60f330ab922
Fedi Ghalloussi <62482405+FediGhalloussi@users.noreply.github.com>
2023-11-26 11:09:45 +0100

    Respawn + transition

--- Assets/Scripts/RespawnManager.cs
+++ Assets/Scripts/RespawnManager.cs
@@ -1,22 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class RespawnManager : MonoBehaviour
 {
-    public float respawnYThreshold = -2f;
+    public float respawnYMinThreshold = -5f;
+    public float respawnYMaxThreshold = 22f;
     public Transform respawnPoint; // Set this to the position where you want the player to respawn.
+    public GameObject player;
 
     void Update()
     {
         // Check the player's Y-coordinate.
-        if (transform.position.y < respawnYThreshold)
+        if (player.transform.position.y < respawnYMinThreshold || player.transform.position.y > respawnYMaxThreshold)
         {
-            RespawnPlayer();
+            StartCoroutine(RespawnPlayer());
         }
     }
 
-    void RespawnPlayer()
+    public IEnumerator RespawnPlayer()
     {
+        StartCoroutine(GameObject.Find("UIPaint").GetComponent<ChangeSceneAnimation>().doAnimation("Open", null));
+        yield return new WaitForSeconds(.5f);
         // Set the player's position to the respawn point.
-        transform.position = respawnPoint.position;
+        player.transform.position = respawnPoint.position;
+        // reset gravity
+        Physics.gravity = new Vector3(0, -10.0F, 0);
+        Camera.main.transform.parent.rotation = Quaternion.Euler(0f, 0f, 0f);
+        StartCoroutine(GameObject.Find("UIPaint").GetComponent<ChangeSceneAnimation>().doAnimation("Close", null));
+
+        
     }
 }
