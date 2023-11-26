f921156070e1d94f2984371f8bdf93569edbadfc
Mett0t <100679801+Mett0t@users.noreply.github.com>
2023-11-26 11:03:56 +0100

    correction lvl2

--- Assets/unlock_portal.cs
+++ Assets/unlock_portal.cs
@@ -7,8 +7,10 @@ public class unlock_portal : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
+        print("1");
         if (other.CompareTag("Sphere"))
         {
+            print("2");
             Destroy(other);
             Destroy(this);
         }
